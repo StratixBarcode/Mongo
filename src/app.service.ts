cdc913dc60a7b86bdb27fc1705c1724c801f321d
zengmanhuang <zengman.huang@stratixcorp.com>
2023-06-05 16:35:43 -0400

    origin main

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,11 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return this.v1_Hello();
-  }
-
-  v1_Hello(): string {
-    return 'Hello World v1!';
+    return 'Hello World!';
   }
   
 }
