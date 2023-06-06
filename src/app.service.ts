eb067c60778ab7e504b372c8b76ae1c2b45f77df
zengmanhuang <zengman.huang@stratixcorp.com>
2023-06-06 17:09:34 -0400

    Add mainTest

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,12 +3,18 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const hello = this.mainHello()
+    const hello = this.mainHello888()
     console.log(hello)
     return hello
   }
   
-  mainHello() {
+  mainHello888(): string {
     return 'Hello main 888!';
   }
+
+  mainTest(): number{
+    return 888;
+  }
+
+
 }
